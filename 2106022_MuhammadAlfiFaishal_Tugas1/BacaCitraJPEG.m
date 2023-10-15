3edf23d2d3bfaca5c6caa923f9663ca03a0e151a
alfifaishal <126564908+alfifaishal@users.noreply.github.com>
2023-10-15 21:04:31 +0700

    Add files via upload

---
+++ 2106022_MuhammadAlfiFaishal_Tugas1/BacaCitraJPEG.m
@@ -0,0 +1,7 @@
+%%%%%%%%% Membaca Citra JPEG dan Menampilkan ke Layar
+
+clc; clear; close all;
+
+I = imread('lena.jpeg'); 
+imshow(I);
+whos
---
+++ 2106022_MuhammadAlfiFaishal_Tugas1/CitraGrayscaleJPEG.m
@@ -0,0 +1,10 @@
+%%%%%%%%% Citra Grayscale
+
+clc; clear; close all;
+
+I = imread('lenaGrayscale.jpg');
+imshow(I);
+whos
+
+
+I(20:25, 100:110)
